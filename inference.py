1fcbe2b0446b440a54b4bedd35517bbf9b38b5f7
Jonathan Clark <jonathan@hexa3d.io>
2024-12-05 16:24:26 +0200

    Use CLI inference

---
+++ inference.py
@@ -0,0 +1,44 @@
+import os
+
+os.environ['SPCONV_ALGO'] = 'native'  # Can be 'native' or 'auto', default is 'auto'.
+# 'auto' is faster but will do benchmarking at the beginning.
+# Recommended to set to 'native' if run only once.
+import argparse
+from PIL import Image
+from trellis.pipelines import TrellisImageTo3DPipeline
+from trellis.utils import postprocessing_utils
+
+
+def run(args):
+    # Load a pipeline from a model folder or a Hugging Face model hub.
+    pipeline = TrellisImageTo3DPipeline.from_pretrained("JeffreyXiang/TRELLIS-image-large")
+    pipeline.cuda()
+
+    # Load an image
+    image = Image.open(args.image_path)
+
+    # Run the pipeline
+    outputs = pipeline.run(
+        image,
+        seed=555,
+    )
+
+    # GLB files can be extracted from the outputs
+    glb = postprocessing_utils.to_glb(
+        outputs['gaussian'][0],
+        outputs['mesh'][0],
+        # Optional parameters
+        simplify=0.95,  # Ratio of triangles to remove in the simplification process
+        texture_size=1024,  # Size of the texture used for the GLB
+    )
+    glb.export(os.path.join(args.output_dir, 'output.glb'))
+
+
+if __name__ == "__main__":
+    # Parse arguments and then call run
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image_path', type=str, required=True)
+    parser.add_argument('--output_dir', type=str, required=True)
+    args = parser.parse_args()
+
+    run(args)
